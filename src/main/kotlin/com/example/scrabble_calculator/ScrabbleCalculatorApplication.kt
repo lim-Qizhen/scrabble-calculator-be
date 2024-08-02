8a6a97aba1f633ce9e6e64becb29dfecfcc992f4
Qizhen <qlim@palo-it,com>
2024-08-02 15:41:23 +0800

    feat: Enable cors

--- src/main/kotlin/com/example/scrabble_calculator/ScrabbleCalculatorApplication.kt
+++ src/main/kotlin/com/example/scrabble_calculator/ScrabbleCalculatorApplication.kt
@@ -2,10 +2,18 @@ package com.example.scrabble_calculator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class ScrabbleCalculatorApplication
+@EnableWebMvc
+class ScrabbleCalculatorApplication : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<ScrabbleCalculatorApplication>(*args)
+    runApplication<ScrabbleCalculatorApplication>(*args)
 }
